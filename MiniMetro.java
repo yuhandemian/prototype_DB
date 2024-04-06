e122d00faf2ba0b77f8b51b2089997532b0de57b
LimitedPark <yuhn1011@naver.com>
2024-04-06 16:30:20 +0900

    first_upload

---
+++ MiniMetro.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MiniMetro extends JFrame {
+
+    public MiniMetro(){
+        setTitle("Mini Metro Simulator");
+
+        setSize(300,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setLayout(null);
+
+    }
+
+    public static void main(String[] args){
+        new MiniMetro();
+    }
+}
---
+++ eventexextends.java
@@ -0,0 +1,42 @@
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Event Listener Example 1
+ * 독립적인 클래스로 리스너 생성
+ * https://thinkground.studio
+ * YouTube : Around Hub Studio
+ * @author Flature
+ */
+public class eventexextends JFrame {
+
+    private JButton jButton = new JButton("버튼");
+
+    public EventExample1() {
+
+        setSize(500,500);
+
+        jButton.addActionListener(new ListenerClass());
+        add(jButton);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args){
+        new EventExample1();
+    }
+}
+
+class ListenerClass implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        System.out.println("호출!");
+    }
+
+}
